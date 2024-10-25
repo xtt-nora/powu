35d80ac0e0ec1b1f29818cbfb3666457987b1b4f
DESKTOP-AMTPMOF\xtt <xutingting7307@163.com>
2024-10-25 19:25:08 +0800

    feat: 登录连接后端

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,18 +4,18 @@
       <el-form
         ref="ruleFormRef"
         style="max-width: 600px"
-        :model="ruleForm"
+        :model="loginForm"
         status-icon
         :rules="rules"
         label-width="auto"
         size="small"
         class="demo-ruleForm"
       >
-        <el-form-item label="Name" prop="name">
-          <el-input v-model.number="ruleForm.name" />
+        <el-form-item label="Name" prop="username">
+          <el-input v-model.number="loginForm.username" />
         </el-form-item>
         <el-form-item label="Password" prop="password">
-          <el-input v-model="ruleForm.password" type="password" autocomplete="off" />
+          <el-input v-model="loginForm.password" type="password" autocomplete="off" />
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="submitForm(ruleFormRef)"> Submit </el-button>
@@ -29,25 +29,34 @@
 <script lang="ts" setup>
 import { reactive, ref } from "vue";
 import type { FormInstance, FormRules } from "element-plus";
-
+import { loginApi } from "@/api/modules/login";
+import { useUserStore } from "@/store/modules/user";
+import { useRouter } from "vue-router";
+import { HOME_URL } from "@/config";
+const router = useRouter();
 const ruleFormRef = ref<FormInstance>();
-const ruleForm = reactive({
+const userStore = useUserStore();
+const loginForm = reactive({
   password: "",
-  name: "",
+  username: "",
 });
 
-const rules = reactive<FormRules<typeof ruleForm>>({
+const rules = reactive<FormRules<typeof loginForm>>({
   password: [{ trigger: "blur" }],
-  name: [{ trigger: "blur" }],
+  username: [{ trigger: "blur" }],
 });
 
 const submitForm = (formEl: FormInstance | undefined) => {
   if (!formEl) return;
-  formEl.validate((valid) => {
-    if (valid) {
-      console.log("submit!");
-    } else {
-      console.log("error submit!");
+  formEl.validate(async (valid) => {
+    if (!valid) return;
+    try {
+      // 1.执行登录接口
+      const { data } = await loginApi({ ...loginForm });
+      userStore.setToken(data.access_token);
+      // 4.跳转到首页
+      router.push(HOME_URL);
+    } finally {
     }
   });
 };
