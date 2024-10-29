8a488eceb85dc2f0f26eaaaeb95da278028b7996
DESKTOP-AMTPMOF\xtt <xutingting7307@163.com>
2024-10-29 15:16:44 +0800

    feat: 持久化

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,8 @@
 import type { App } from "vue";
 import { createPinia } from "pinia";
-
+import piniaPersistedState from "pinia-plugin-persistedstate";
 const store = createPinia();
-
+store.use(piniaPersistedState);
 // 全局注册 store
 export function setupStore(app: App<Element>) {
   app.use(store);
--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,13 +1,20 @@
 import { defineStore } from "pinia";
 import { UserState } from "@/store/interface";
-export const useUserStore = defineStore("user", () => {
-  const token = ref<string>("");
-  const userInfo = ref<any>({ name: "Geeker" });
-  function setToken(value: string) {
-    token.value = value;
+import piniaPersistConfig from "../helper/persist";
+export const useUserStore = defineStore(
+  "user",
+  () => {
+    const token = ref<string>("");
+    const userInfo = ref<any>({ name: "Nora" });
+    function setToken(value: string) {
+      token.value = value;
+    }
+    function setUserInfo(value: UserState["userInfo"]) {
+      userInfo.value = value;
+    }
+    return { token, userInfo, setToken, setUserInfo };
+  },
+  {
+    persist: piniaPersistConfig("user"),
   }
-  function setUserInfo(value: UserState["userInfo"]) {
-    userInfo.value = value;
-  }
-  return { token, userInfo, setToken, setUserInfo };
-});
+);
