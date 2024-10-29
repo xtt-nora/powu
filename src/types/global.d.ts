ec90d5aa24c37526a3b63916c0b36b61f3c4b56a
DESKTOP-AMTPMOF\xtt <xutingting7307@163.com>
2024-10-29 14:01:03 +0800

    feat: ts规则

---
+++ src/types/global.d.ts
@@ -0,0 +1,24 @@
+declare const __APP_INFO__: {
+  pkg: {
+    name: string;
+    version: string;
+    // 其他需要的字段
+  };
+};
+declare interface ViteEnv {
+  VITE_USER_NODE_ENV: "development" | "production" | "test";
+  VITE_GLOB_APP_TITLE: string;
+  VITE_PORT: number;
+  VITE_OPEN: boolean;
+  VITE_REPORT: boolean;
+  VITE_ROUTER_MODE: "hash" | "history";
+  VITE_BUILD_COMPRESS: "gzip" | "brotli" | "gzip,brotli" | "none";
+  VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE: boolean;
+  VITE_DROP_CONSOLE: boolean;
+  VITE_PWA: boolean;
+  VITE_DEVTOOLS: boolean;
+  VITE_PUBLIC_PATH: string;
+  VITE_API_URL: string;
+  VITE_PROXY: [string, string][];
+  VITE_CODEINSPECTOR: boolean;
+}
